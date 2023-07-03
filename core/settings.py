74e2aadbc6ba0063bc0eb79db033c18f3ef5d33f
GabJS10 <gballesteros@uniguajira.edu.co>
2023-07-03 16:38:46 -0500

    add mediagionroot

--- core/settings.py
+++ core/settings.py
@@ -134,7 +134,7 @@ if not DEBUG:
     # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/media_root')
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
